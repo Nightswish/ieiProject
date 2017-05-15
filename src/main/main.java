47dd1c2a223ffe74f6218d313b1a29f1d04cb1f9
준구 <whangjg@naver.com>
2017-05-15 18:40:10 +0900

    비로그인, 좌석선택시 로그인 하라는 다이얼로그 추가

---
+++ src/main/main.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import ieiProject.TotalTicket_sub;
+
+public class main {
+	public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
+		TotalTicket_sub ex = new TotalTicket_sub();
+	}
+}
